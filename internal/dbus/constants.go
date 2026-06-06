8dd697f16cc92f0b3173e3e51120f1532d333dfe
thecentinol <centinol@proton.me>
2026-06-06 22:10:04 +0200

    chore(dbus): add access point constants

--- internal/dbus/constants.go
+++ internal/dbus/constants.go
@@ -6,4 +6,13 @@ const (
 	deviceType      = baseServiceName + ".Device.DeviceType"
 	deviceWireless  = baseServiceName + ".Device.Wireless"
 	deviceWired     = baseServiceName + ".Device.Wired"
+
+	accessPointFlags    = baseServiceName + ".AccessPoint.Flags"
+	accessPointWpaFlags = baseServiceName + ".AccessPoint.WpaFlags"
+	accessPointRsnFlags = baseServiceName + ".AccessPoint.RsnFlags"
+	accessPointSsid     = baseServiceName + ".AccessPoint.Ssid"
+	accessPointFreq     = baseServiceName + ".AccessPoint.Frequency"
+	accessPointBssid    = baseServiceName + ".AccessPoint.HwAddress"
+	accessPointMode     = baseServiceName + ".AccessPoint.Mode"
+	accessPointStrength = baseServiceName + ".AccessPoint.Strength"
 )
