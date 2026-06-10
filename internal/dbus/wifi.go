27ee1df5d236501a1ecbfeaf7f8f3b075a9f61b9
thecentinol <centinol@proton.me>
2026-06-10 23:47:22 +0200

    refactor(dbus): rename device.go -> wifi.go
    This removes the abiguity of what the file is actually for.

--- internal/dbus/wifi.go
+++ internal/dbus/wifi.go
@@ -229,7 +229,7 @@ func GetActiveNetwork(c *Client) (*models.AccessPoint, error) {
 
 	devicePath, err := GetWifiDevice(c)
 	if err != nil {
-		return nil, err // NOTE: is return type correct
+		return nil, err
 	}
 	device := c.conn.Object(baseServiceName, devicePath)
 	active, err := device.GetProperty(activeAccessPoint)
