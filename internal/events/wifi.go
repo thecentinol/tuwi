4dd8364dc34ac8ef84153f3191f3475901133349
thecentinol <centinol@proton.me>
2026-07-05 03:23:42 +0200

    chore(events): return SavedConnection in SavedWifiMsg

--- internal/events/wifi.go
+++ internal/events/wifi.go
@@ -4,7 +4,7 @@ import (
 	nm "github.com/thecentinol/tuwi/internal/networkmanager"
 )
 
-type SavedWifiMsg struct{ Networks []nm.AccessPoint }
+type SavedWifiMsg struct{ Networks []nm.SavedConnection }
 type AvailableWifiMsg struct{ Networks []nm.AccessPoint }
 type WifiConnectReqMsg struct {
 	Network  *nm.AccessPoint
